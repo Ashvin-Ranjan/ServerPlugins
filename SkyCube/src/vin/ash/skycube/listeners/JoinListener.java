e4c5c617ce27c1a3c4b840d8d1817a7038836c8c
Ashvin-Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2020-12-04 09:41:22 -0800

    Island Generation

--- SkyCube/src/vin/ash/skycube/listeners/JoinListener.java
+++ SkyCube/src/vin/ash/skycube/listeners/JoinListener.java
@@ -1,5 +1,9 @@
 package vin.ash.skycube.listeners;
 
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -32,15 +36,22 @@ public class JoinListener implements Listener{
 			if(plugin.getServer().getOnlinePlayers() != null) {
 				if(plugin.getServer().getOnlinePlayers().size() != 0) {
 					//If there is an online player spawn the new one within 6 chunks of their spawn
-					//TODO Finish spawn finder
+					Location spawn = findSpawnLocation(p, plugin.getServer().getOnlinePlayers(), Utils.getAllPlayers(Bukkit.getServer()));
+					p.setBedSpawnLocation(spawn);
+					p.teleport(spawn);
+					generateIsland(spawn);
+					
 				} else if(plugin.getServer().getOfflinePlayers() != null ) {
 					//If there are no online players then spawn the new one within 6 chunks of a offline players spawn
-					//TODO Finish spawn finder
+					Location spawn = findSpawnLocation(p, Utils.toOnlinePlayer(plugin.getServer().getOfflinePlayers()));
+					p.setBedSpawnLocation(spawn);
+					p.teleport(spawn);
+					generateIsland(spawn);
 				} else {
 					//If this is the first player then spawn them at 0, 0, 0
-					p.setBedSpawnLocation(new Location(Bukkit.getServer().getWorlds().get(0), 0, 0, 0));
-					p.teleport(new Location(Bukkit.getServer().getWorlds().get(0), 0, 0, 0));
-					generateIsland(new Location(Bukkit.getServer().getWorlds().get(0), 0, 0, 0));
+					p.setBedSpawnLocation(new Location(Bukkit.getServer().getWorlds().get(0), 0, 0, 65));
+					p.teleport(new Location(Bukkit.getServer().getWorlds().get(0), 0, 0, 65));
+					generateIsland(new Location(Bukkit.getServer().getWorlds().get(0), 0, 0, 65));
 				}
 			}
 			
@@ -87,6 +98,52 @@ public class JoinListener implements Listener{
 		w.getBlockAt(x+1, y-3, z+1).setBlockData(Material.LAVA.createBlockData());
 		w.getBlockAt(x, y-3, z-1).setBlockData(Material.STONE.createBlockData());
 		w.getBlockAt(x, y-3, z+1).setBlockData(Material.STONE.createBlockData());
+	}
+	
+	Location findSpawnLocation(Player p, Player[] playerlist) {
+		Location endloc;
+		Random r = new Random(Calendar.getInstance().getTimeInMillis());
+		Player spawnp = (Player) playerlist[r.nextInt(playerlist.length)];
+		boolean correctspawn = false;
+		do {
+			boolean fail = false;
+			endloc = spawnp.getBedSpawnLocation().add(new Location(Bukkit.getServer().getWorlds().get(0), r.nextInt(25) + 25, r.nextInt(25) + 25, 0));
+			for (Player pl : playerlist){
+				if(Utils.getDist(endloc, pl.getBedSpawnLocation()) < 15) {
+					spawnp = pl;
+					fail = true;
+					break;
+				}
+			}
+			if(!fail) {
+				correctspawn = true;
+			}
+		} while (!correctspawn);
+		
+		return endloc;
+	}
+	
+	Location findSpawnLocation(Player p, Collection<? extends Player> playerlist, Player[] full) {
+		Location endloc;
+		Random r = new Random(Calendar.getInstance().getTimeInMillis());
+		Player spawnp = (Player) playerlist.toArray()[r.nextInt(playerlist.size())];
+		boolean correctspawn = false;
+		do {
+			boolean fail = false;
+			endloc = spawnp.getBedSpawnLocation().add(new Location(Bukkit.getServer().getWorlds().get(0), r.nextInt(25) + 25, r.nextInt(25) + 25, 0));
+			for (Player pl : full){
+				if(Utils.getDist(endloc, pl.getBedSpawnLocation()) < 15) {
+					spawnp = pl;
+					fail = true;
+					break;
+				}
+			}
+			if(!fail) {
+				correctspawn = true;
+			}
+		} while (!correctspawn);
+		
+		return endloc;
 	}
 	
 }
--- SkyCube/src/vin/ash/skycube/utils/Utils.java
+++ SkyCube/src/vin/ash/skycube/utils/Utils.java
@@ -1,11 +1,33 @@
 package vin.ash.skycube.utils;
 
-import net.md_5.bungee.api.ChatColor;
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+
 
 public class Utils {
 	
 	public static String chat (String s) {
 		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+	
+	public static double getDist(Location a, Location b) {
+		return Math.sqrt(Math.pow(a.getX() - b.getX(), 2)+Math.pow(a.getZ() - b.getZ(), 2));
+	}
+	
+	public static Player[] getAllPlayers(Server s) {
+		return (Player[]) ArrayUtils.addAll(s.getOnlinePlayers().toArray(), toOnlinePlayer(s.getOfflinePlayers()));
+	}
+	
+	public static Player[] toOnlinePlayer(OfflinePlayer[] p) {
+		Player[] pl = new Player[p.length];
+		for(int i = 0; i < p.length; i++) {
+			pl[i] = p[i].getPlayer();
+		}
+		return pl;
 	}
 	
 }
